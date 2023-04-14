7dde1b41c20a2c94b51572d05d4b020ccf2c9c39
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-04-14 17:44:00 -0300

    add update page

--- src/js/controller/edit_client_controller.js
+++ src/js/controller/edit_client_controller.js
@@ -1,4 +1,19 @@
+const btEdit = document.querySelector(".btn_editar");
+const table = document.querySelector(".table");
 
+table.addEventListener("click", ( e ) => {
+  const event = e.target.className === "btn btn_editar";
+
+  if( event ){
+    window.open("http://127.0.0.1:5500/src/pages/update.html")
+  }
+  
+});
+
+
+// btEdit.addEventListener("click", () => {
+//   
+// });;
 
 
 
