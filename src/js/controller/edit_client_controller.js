365186207b605614f347e359fc21e8504d92d6b2
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-04-14 18:04:48 -0300

    add structure update html

--- src/js/controller/edit_client_controller.js
+++ src/js/controller/edit_client_controller.js
@@ -1,19 +1,7 @@
-const btEdit = document.querySelector(".btn_editar");
-const table = document.querySelector(".table");
+const getUrl = window.location;
 
-table.addEventListener("click", ( e ) => {
-  const event = e.target.className === "btn btn_editar";
+console.log( getUrl );
 
-  if( event ){
-    window.open("http://127.0.0.1:5500/src/pages/update.html")
-  }
-  
-});
-
-
-// btEdit.addEventListener("click", () => {
-//   
-// });;
 
 
 
--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,19 @@ import {  newClient } from "./controller/add_client_controller.js";
 import {  deleteClient } from "./controller/delete_client_controller.js";
 import { editClient } from "./services/clients_services.js";
 
+const btEdit = document.querySelector(".btn_editar");
+const table = document.querySelector(".table");
+
+
+table.addEventListener("click", ( e ) => {
+  const event = e.target.className === "btn btn_editar";
+  
+  if( event ){
+    window.open("http://127.0.0.1:5500/src/pages/update.html");
+  }
+  
+});
+
 
 
 getClient();
