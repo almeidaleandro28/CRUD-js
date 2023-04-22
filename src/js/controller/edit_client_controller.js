d22024d9f650df65c298bf87dedc2aa2fb4134e8
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-04-22 10:37:03 -0300

    feat - update edit client
    add data in the inputs of getClient

--- src/js/controller/edit_client_controller.js
+++ src/js/controller/edit_client_controller.js
@@ -1,1 +1,15 @@
+import { getClient, editClient } from "../services/clients_services.js"; 
+
+
+const getUrl = new URL( window.location );
+
+const getId = getUrl.searchParams.get("id");
+
+const $inputName = document.querySelector("[data-name]");
+const $inputEmail = document.querySelector("[data-email]");
+
+getClient( getId ).then( 
+  (data) => {
+          $inputName.value = data.nome
+          $inputEmail.value = data.email })
 
