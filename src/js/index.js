911b68c8f7ce91106289ed5ca294277cba14233c
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-04-25 19:06:55 -0300

    feat - add update html
    show message update finished

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,6 @@ import { getClient, createLine } from "./controller/clients_controller.js";
 import {  newClient } from "./controller/add_client_controller.js";
 import {  deleteClient } from "./controller/delete_client_controller.js";
 
-
-
-
 getClient();
 createLine();
 newClient();
